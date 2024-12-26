675aeeab56b27f652ad54f94c6d4698f9e00283d
VuDangKhoa <dangkhoa09102004@gmail.com>
2024-12-26 14:56:58 +0700

    add db.js configure_db v1

---
+++ Source/backend/db.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/elearning';
+
+console.log("Connecting to MongoDB URI:", mongoURI);
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log("Database connected successfully!");
+    })
+    .catch((err) => {
+        console.error("Error connecting to database:", err);
+        process.exit(1); // Thoát chương trình nếu không kết nối được
+    });
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.error("MongoDB Connection Error:", error));
+db.once("open", () => console.log("MongoDB connection is open."));
+
+module.exports = {
+    db
+};
